17ab8d20841249f966a5f5b451d5e4e2d19ea1a3
Vivi <vivi@MacBook-Pro-de-Caju.local>
2020-12-09 19:46:19 -0300

    ft/initial settings

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'prettier',
+        'prettier/react',
+    ],
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier', 'react-hooks'],
+    rules: {
+        'prettier/prettier': 'error',
+        'react/jsx-filename-extension': [
+            'warn',
+            { extensions: ['.jsx', '.js'] },
+        ],
+        'import/prefer-default-export': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+    },
+};
